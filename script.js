a5cf77e668498cd36bdfd3082dd528559e581015
Isabel Acevedo <acev0010@algonquinlive.com>
2023-05-02 13:22:26 -0400

    app is finished

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.textContent  = count
+}
+
+function save() {
+    let saveCount = count + " - "
+    saveEl.textContent += saveCount
+    countEl.textContent = 0
+    count = 0
+    console.log(saveCount)
+}
+
